9ff8a3175757a4eadeb6c54bf98222dab6205148
ozakboy <ozakboy@gmail.com>
2022-03-27 15:13:23 +0800

    新增 快速轉換方式

--- Ozakboy.PageData/Ozakboy.PageData/ToPageData.cs
+++ Ozakboy.PageData/Ozakboy.PageData/ToPageData.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Ozakboy.PageData
 {
-    internal class ToPageData
+    public static class ToPageDataHelper
     {
+        public static  VPageData<T> ToPageData<T>(this List<T> _Data, int _page, int _Limit, int _total)
+        {
+             return new VPageData<T>(_Data,_page,_Limit,_total);
+        }
+        public static VPageData<T> ToPageData<T>(this List<T> _Data, int _page, int _Limit)
+        {
+            return new VPageData<T>(_Data, _page, _Limit);
+        }
+        public static VPageData<T> ToPageData<T>(this IQueryable<T> _Data, int _page, int _Limit, int _total)
+        {
+            return new VPageData<T>(_Data, _page, _Limit,  _total);
+        }
+        public static VPageData<T> ToPageData<T>(this IQueryable<T> _Data, int _page, int _Limit)
+        {
+            return new VPageData<T>(_Data, _page, _Limit);
+        }
+
+        public static VPageData<T> ToPageData<T>(this IEnumerable<T> _Data, int _page, int _Limit, int _total)
+        {
+            return new VPageData<T>(_Data, _page, _Limit, _total);
+        }
+        public static VPageData<T> ToPageData<T>(this IEnumerable<T> _Data, int _page, int _Limit)
+        {
+            return new VPageData<T>(_Data, _page, _Limit);
+        }
     }
 }
